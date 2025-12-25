15f68691048fd496254328c203a649ffb76cece1
RakPlayzzz21 <rakshitplayzzz21@gmail.com>
2025-12-25 18:53:07 +0530

    Add files via upload

---
+++ Task_1.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+sum = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+quotient = num1 / num2
+
+print("Addition:", sum)
+print("Subtraction:", difference)
+print("Multiplication:", product)
+print("Division:", quotient)
